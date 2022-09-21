a11448702da31620de68c64911d28c2a52d2d8cd
Wellinton Rafael Gudilunas <wellintongudilunas788@gmail.com>
2022-09-21 17:52:03 -0300

    Commit Projeto inteiro com CSS desatualizado

---
+++ PROJETO A1/js/PROJETO1.js
@@ -0,0 +1,71 @@
+function NOTAS () {
+    var PrimeiraNota = document.querySelector("input#Nota1")
+    var SegundaNota  = document.querySelector("input#Nota2")
+    var TerceiraNota = document.querySelector("input#Nota3")
+    var QuartaNota   = document.querySelector("input#Nota4")
+    var QuintaNota   = document.querySelector("input#Nota5")
+    var SextaNota    = document.querySelector("input#Nota6")
+    var res = window.document.querySelector("div#res")
+    var n1  = Number(PrimeiraNota.value)
+    var n2  = Number(SegundaNota.value) 
+    var n3  = Number(TerceiraNota.value) 
+    var n4  = Number(QuartaNota.value) 
+    var n5  = Number(QuintaNota.value) 
+    var n6  = Number(SextaNota.value)
+    var MediaNotas= (n1 + n2 + n3 + n4 + n5 + n6) / 6
+
+    //MAIOR NOTA
+
+    var notas = [n1, n2, n3, n4, n5, n6]
+
+    maiorValor = 0
+
+    for (var indice = 0; indice < notas.length; indice++) {
+        if (maiorValor < notas[indice]){
+            maiorValor = notas[indice]
+        }
+    }
+
+    //MENOR NOTA
+    var menorValor = 1000 
+
+    for (var indice = 0; indice < notas.length; indice++) {
+    if (menorValor > notas[indice]){
+    menorValor = notas[indice]
+    }
+    }
+
+function verificaSituacao (nota) {
+    var situacao
+    if (nota >= 7) {
+        situacao = "<strong>Aprovado</strong>"
+    } else if (nota <=5) {
+        situacao = "<strong>Reprovado</strong>"
+    }else {
+        situacao = "<strong>Em Exame</strong>"
+    }
+    return situacao
+}
+
+    // APROVADO OU REPROVADO
+    situacao1 = verificaSituacao(n1)
+    situacao2 = verificaSituacao(n2)
+    situacao3 = verificaSituacao(n3)
+    situacao4 = verificaSituacao(n4)
+    situacao5 = verificaSituacao(n5)
+    situacao6 = verificaSituacao(n6)
+
+    Situacao.innerHTML=
+    "O primeiro aluno está: "  + situacao1 + "<br>" +
+    " O segundo aluno está: "  + situacao2 + "<br>" + 
+    " O terceiro aluno está:"  + situacao3 + "<br>" + 
+    " O quarto aluno está:  "  + situacao4 + "<br>" + 
+    " O quinto aluno está:  "  + situacao5 + "<br>" + 
+    " O sexto aluno está:   "  + situacao6
+    
+    MaiorNota.innerHTML = "A <strong>Maior</strong> nota digitada foi: " + maiorValor
+    MenorNota.innerHTML = "A <strong>Menor</strong> nota digitada foi: " + menorValor
+    res.innerHTML =  "As <strong>notas</strong> digitadas foram: " + n1 +
+    ", " + n2 + ", " + n3 + ", " + n4+ ", " + n5 + ", " + n6 
+    Media.innerHTML = "A <strong>Média</strong> das notas é igual a " + MediaNotas
+}
---
+++ PROJETO A1/js/PROJETO2.js
@@ -0,0 +1,86 @@
+    var btGerar = document.querySelector("button#btGerar")
+    var btverifica = document.querySelector("button#btverifica")
+    var txt = document.querySelector("div#sla")
+    var chute = document.querySelector("input#chute")
+    var tentativatxt = document.querySelector("div#txttentativa") 
+    var txtclassificacao = document.querySelector("div#classificacao")
+    var chuteNum = Number(chute)
+    var numeroSorteado = 0;
+    var tentativas = 0;
+    btverifica.addEventListener("click", verifica);
+    btGerar.addEventListener("click", gerar);
+        
+
+    function gerar() {
+            
+        numeroSorteado =  Math.round(Math.random()*100)
+        console.log(numeroSorteado)
+        sla.innerHTML = 'O número mágico já foi sorteado, qual você acha que é ?'
+        btGerar.disabled = true;
+        btverifica.disabled = false;
+    }
+        
+    function reiniciar() {
+
+        btverifica.innerHTML = "Enviar chute"
+        btverifica.removeEventListener("click", reiniciar);
+        btverifica.addEventListener("click", verifica);
+        btverifica.disabled = false;
+        resultado.innerHTML = " "
+        txttentativa.innerHTML = " "
+        txtclassificacao.innerHTML = " "
+        chute.value = ""
+        gerar();
+        tentativas = 0;
+        sla.innerHTML = "Outro número já foi sorteado, qual você acha que é agora? "
+        }
+
+        
+    function verifica() {
+
+        if (numeroSorteado == chute.value) {
+        tentativas++
+        resultado.innerHTML = 'Parabéns, você Acertou na sua '
+        txttentativa.innerHTML = tentativas +"° tentativa"
+        btverifica.innerHTML = "Tentar Novamente?"
+        btverifica.removeEventListener("click", verifica);
+        btverifica.addEventListener("click", reiniciar);;
+
+            switch (tentativas) {
+                case 1: 
+                case 2:
+                    txtclassificacao.innerHTML = "Você foi classificado com 5 ESTRELAS";
+                    break;
+                case 3:
+                case 4:
+                case 5: 
+                    txtclassificacao.innerHTML = "Você foi classificado com 4 ESTRELAS";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    txtclassificacao.innerHTML = "Você foi classificado com 3 ESTRELAS";
+                    break;
+                case 9:
+                case 10:
+                    txtclassificacao.innerHTML = "Você foi classificado com 2 ESTRELAS";
+                    break;
+                default:
+                    txtclassificacao.innerHTML = "Você foi classificado com 1 ESTRELA :(";
+                    break;
+                
+            }
+            } else if (numeroSorteado > chute.value){ 
+                tentativas++ + 1
+                txttentativa.innerHTML = "Tentativa: " + tentativas;
+                resultado.innerHTML = 'Você Errou e o numero sorteado é MAIOR que ' + chute.value
+            } else if (numeroSorteado < chute.value) {
+                tentativas++
+                txttentativa.innerHTML = "Tentativa: " + tentativas;
+                resultado.innerHTML = `Você errou e o número sorteado é MENOR que ${chute.value}`
+            }
+        }
+
+
+    
+    
